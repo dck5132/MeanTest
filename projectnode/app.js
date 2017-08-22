6c963d25bb94ae9bae78b4ff75d8acaf4c2eb68b
Cyril Kalikin <dck5132@gmail.com>
2017-08-22 20:02:51 +0000

    Added a gitignore, reorganized the directory, added bootstrap template, added public & src & views folders to projectnode, deleted default files that were not utilzied

--- projectnode/app.js
+++ projectnode/app.js
@@ -1,3 +1,17 @@
-var test = "First Variable";
+var express = require('express');
+var app = express();
+var port = process.env.PORT;
 
-console.log(test);
+app.get("/", function(req,res,next){
+    res.send("Success");
+});
+
+app.get("/routing", function(req,res,next){
+    res.send("Routing Success!");
+});
+app.listen(port, function(err) {
+    if(err){
+        throw err;
+    }
+    console.log("Server is running on " + port);
+})
