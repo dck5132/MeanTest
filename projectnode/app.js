4cb2ed1823c3dcb5619d32eccdd13a41084d0047
Cyril Kalikin <dck5132@gmail.com>
2017-08-28 14:49:42 +0000

    +Renamed routes & files

--- projectnode/app.js
+++ projectnode/app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/events', event);
+app.use('/event', event);
 //app.use('/users', users);
 
 app.get("/routing", function(req,res,next){
